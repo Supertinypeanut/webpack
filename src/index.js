3e0b44fdde2d44ef1fa25fcd61ea837fb8bfae26
supertinypeaunt <1176032017@qq.com>
2019-11-30 01:36:37 +0800

    懒加载

--- src/index.js
+++ src/index.js
@@ -11,25 +11,42 @@ import './style/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 // 引入math方法
-import { cube } from './js/math.js';
+// import { cube } from './js/math.js';
 
-// const path = require('path')
-import a from '@/js/a.js'
-a()
-console.log('我手机')
+import _ from 'lodash';
+
+
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
+// function component() {
+//   var element = document.createElement('pre');
+//   element.innerHTML = [
+//     'Hello webpack!',
+//     '5 cubed is equal to ' + cube(5)
+//   ].join('\n\n');
+//   return element;
+// }
+
 function component() {
-  var element = document.createElement('pre');
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ].join('\n\n');
-  return element;
+var element = document.createElement('div');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+  button.innerHTML = 'Click me and look at the console!';
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './js/print').then(module => {
+    var print = module.default;
+    print();
+  });
+ return element;
 }
+
 document.body.appendChild(component());
 
 new Vue({
